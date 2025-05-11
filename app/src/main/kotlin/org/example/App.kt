418ef7ac5d5e94e6db97fc73103cb6d3c01ca027
Facu2009 <fana.rodriguez@alumno.etec.um.edu.ar>
2025-05-11 19:07:25 -0300

    Etapa 2: Funciones con Multiples Parametros

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -71,12 +71,17 @@ fun esAprobado(nota: Double): Boolean {
 // Etapa 2
 fun calcularPromedioTresNotas(nota1: Double, nota2: Double, nota3: Double): Double {
     // Implementar aquí
-    return 0.0
+    return (nota1 + nota2 + nota3) / 3 
 }
 
 fun obtenerEstadoAlumno(nombre: String, apellido: String, nota: Double): String {
     // Implementar aquí
-    return ""
+    if (nota >= 6.0){
+        return "El alumno ${nombre} ${apellido} esta aprobado"
+    } else {
+        return "El alumno ${nombre} ${apellido} esta desaprobado"
+    }
+    
 }
 
 // Etapa 3
